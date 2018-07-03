089b92bae515dcf764969d07830ed5e09fc794f3
Karl Hasselbring <karl.hasselbring@gmx.de>
2018-07-03 13:29:04 +0200

    12er node

--- Script3.sh
+++ Script3.sh
@@ -2,7 +2,7 @@
 #PBS -N MPI
 #PBS -j oe
 #PBS -m ae
-#PBS -l nodes=4:ppn=4
+#PBS -l nodes=1:ppn=12
 #PBS -l walltime=0:00:30
 #PBS -l mem=1gb
 #PBS -W x=PARTITION:tane
@@ -13,6 +13,6 @@ module load foss
 
 make full
 
-mpirun -np 16 --map-by node -display-map -display-allocation  ./main > log3.txt
+mpirun -np 12 --map-by node -display-map -display-allocation  ./main > log3.txt
 
 ./create_video.sh

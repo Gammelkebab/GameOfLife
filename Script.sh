563f7ff6e874cfb51a64a9103e105134c3727018
Karl Hasselbring <karl.hasselbring@gmx.de>
2018-07-31 08:32:00 +0200

    Script

--- Script.sh
+++ Script.sh
@@ -1,18 +1,19 @@
 #!/bin/bash -login
-#PBS -N MPI
+#PBS -N GameOfLife
 #PBS -j oe
 #PBS -m ae
-#PBS -l nodes=4:ppn=16
+#PBS -l nodes=4:ppn=4
 #PBS -l walltime=0:00:30
-#PBS -l mem=1gb
+#PBS -l mem=256mb
 #PBS -W x=PARTITION:lena
 
 cd $BIGWORK/GameOfLife
 
 module load intel
 
-make full
+make clean
+make
+
+lfs setstripe --count -1 images
 
 mpirun ./main
-
-./create_video.sh

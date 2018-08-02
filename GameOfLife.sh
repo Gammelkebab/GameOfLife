c8ec45c78a66b8dc7556d251e49f884dd4594379
Aria Givi <ariagivi@yahoo.de>
2018-08-02 13:44:47 +0200

    Renamed Scripts, program will be run in $BIGWORK, changed default values for node and processor count

--- GameOfLife.sh
+++ GameOfLife.sh
@@ -1,8 +1,8 @@
-#!/bin/bash -login
+ #!/bin/bash -login
 #PBS -N GameOfLife
 #PBS -j oe
 #PBS -m ae
-#PBS -l nodes=4:ppn=4
+#PBS -l nodes=4:ppn=16
 #PBS -l walltime=0:00:30
 #PBS -l mem=256mb
 #PBS -W x=PARTITION:lena

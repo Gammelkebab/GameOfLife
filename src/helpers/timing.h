05154214346e05e591f092fc9b7df1be8bb86fd5
Karl Hasselbring <karl.hasselbring@gmx.de>
2018-08-07 11:11:10 +0200

    ^Shithole

--- src/helpers/timing.h
+++ src/helpers/timing.h
@@ -10,12 +10,12 @@ inline void start_timer(struct timeval *begin)
     gettimeofday(begin, NULL);
 }
 
-inline void print_time_since(struct timeval begin)
+inline void print_time_since(struct timeval *begin)
 {
     struct timeval end;
 
     gettimeofday(&end, NULL);
-    double elapsed = (end.tv_sec - begin.tv_sec) + ((end.tv_usec - begin.tv_usec) / 1000000.0);
+    double elapsed = (end.tv_sec - begin->tv_sec) + ((end.tv_usec - begin->tv_usec) / 1000000.0);
     printf("Elapsed: %.5fs\n", elapsed);
 }
 
